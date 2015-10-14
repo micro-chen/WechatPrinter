c584a5787a13886058a3969878c67137b2d43197
xJoeWoo <xjoewoo@hotmail.com>
2015-10-14 18:00:47 +0800

    Bugs Fixed

--- WechatPrinter/UI/MainWindow.xaml.cs
+++ WechatPrinter/UI/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WechatPrinter
 {
@@ -25,24 +12,24 @@ namespace WechatPrinter
         public MainWindow()
         {
             InitializeComponent();
-            this.WindowState = WindowState.Maximized;
-            this.WindowStyle = WindowStyle.None;
-            this.ResizeMode = System.Windows.ResizeMode.NoResize;
-            //this.Topmost = true;
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.None;
+            ResizeMode = ResizeMode.NoResize;
+            Topmost = true;
 
         }
         public void LoadCompleted(MainPage page, WechatPrinterServer server)
         {
-            this.Content = page;
+            Content = page;
             page.Opacity = 0d;
-            page.BeginAnimation(Page.OpacityProperty, new DoubleAnimation(1d, TimeSpan.FromMilliseconds(1000)));
+            page.BeginAnimation(OpacityProperty, new DoubleAnimation(1d, TimeSpan.FromMilliseconds(1000)));
 
             server.StartCheckPrintImg();
         }
 
         private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
         {
-            this.Content = new LoadingPage(this);
+            Content = new LoadingPage(this);
         }
 
         private void MainWindow1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
